6cec4c6503d86feff7fa9e21f16b98455b9b629e
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-03-31 20:47:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// var name1 = "Saad"
+// var name2  = "Rafeh"
+// var name3 = "Usama"
+
+// var students = ["Saad","Rafeh",2,"Faisal"]
+// console.log(students[0][1])
+
+// var name1 = []
+
+// name1[0] = "Yasir"
+// name1[1] = "Ushna"
+// name1[2] = "Areeb"
+
+// console.log(name1[0])
+
+// var names = ["Asad","Sumbal","Areeb","Saad","Maaz","Irshad"]
+// names.pop()
+// console.log(names)
+// names.pop()
+// console.log(names)
+
+// var names = ["Asad","Sumbal","Areeb","Saad","Maaz","Irshad"]
+// names.push("Zeeshan","Aisha")
+// console.log(names)
+
+// var names = ["Asad","Sumbal","Areeb","Saad","Maaz","Irshad"]
+// names.shift()
+// console.log(names)
+
+// var names = ["Asad","Sumbal","Areeb","Saad","Maaz","Irshad"]
+// names.unshift("Noor",1)
+// console.log(names)
+
+// var names = ["Asad","Sumbal","Areeb","Saad","Maaz","Irshad"]
+// names.splice(1,2,"Areeb")
+// console.log(names)
+
+// var names = ["Asad","Sumbal","Areeb","Saad","Maaz","Irshad"]
+// var new1 = names.slice(0,4)
+// console.log(new1)
+
+var names = ["Asad","Sumbal","Areeb","Saad","Maaz","Irshad"]
+var name2 = names.slice(1,5)
+console.log(name2)
+
+
+
